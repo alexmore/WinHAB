3d8fce3694c8c8f2a4ec0eec7d6a3219e8ecb416
alexmore <alexandr.a.mordvinov@gmail.com>
2015-02-06 23:52:04 +0600

    Desktop: Added ViewModel base class and WaiterViewModel

---
+++ source/WinHAB.Core/Mvvm/ViewModel.cs
@@ -0,0 +1,22 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Views;
+
+namespace WinHAB.Core.Mvvm
+{
+  public class ViewModel : ViewModelBase
+  {
+    protected ViewModel(INavigationService navigationService)
+    {
+      Navigation = navigationService;
+    }
+
+    WaiterViewModel _waiter = new WaiterViewModel();
+    public WaiterViewModel Waiter
+    {
+      get { return _waiter; }
+      set { _waiter = value; RaisePropertyChanged(() => Waiter); }
+    }
+
+    public INavigationService Navigation { get; protected set; }
+  }
+}
---
+++ source/WinHAB.Core/Mvvm/WaiterViewModel.cs
@@ -0,0 +1,38 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace WinHAB.Core.Mvvm
+{
+  public class WaiterViewModel : ViewModelBase
+  {
+    bool _isVisible;
+    public bool IsVisible
+    {
+      get { return _isVisible; }
+      set { _isVisible = value; RaisePropertyChanged(() => IsVisible); }
+    }
+
+    string _text;
+    public string Text
+    {
+      get { return _text; }
+      set { _text = value; RaisePropertyChanged(() => Text); }
+    }
+
+    public void Show()
+    {
+      IsVisible = true;
+    }
+
+    public void Show(string text)
+    {
+      Text = text;
+      Show();
+    }
+
+    public void Hide()
+    {
+      IsVisible = false;
+      Text = null;
+    }
+  }
+}
