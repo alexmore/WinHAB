bb79c75da3c086f301cc2941a790854aa4d06d24
alexmore <alexandr.a.mordvinov@gmail.com>
2015-02-07 20:29:58 +0600

    [Tests] Added test for AbstractViewModelViewFactory, ConstructorParameters, MvvmExtensions, WaiterViewModel

---
+++ source/WinHAB.Tests/Core/Mvvm/MvvmExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using WinHAB.Core.Mvvm;
+
+namespace WinHAB.Tests.Core.Mvvm
+{
+  [TestClass]
+  public class MvvmExtensionsTests
+  {
+    [TestMethod]
+    public void MvvmExtensionsViewCleanupTest()
+    {
+      bool isCleanupCalled = false;
+
+      var viewMock = new Mock<IView>();
+      viewMock.SetupAllProperties();
+      
+      var viewModelMock = new Mock<IViewModel>();
+      viewModelMock.Setup(x => x.Cleanup()).Callback(() => isCleanupCalled = true);
+
+      viewMock.Object.DataContext = viewModelMock.Object;
+
+      viewMock.Object.Cleanup();
+
+      Assert.IsTrue(isCleanupCalled);
+
+      viewMock.Object.DataContext = null;
+      viewMock.Object.Cleanup();
+      
+      (null as IView).Cleanup();
+    }
+  }
+}
---
+++ source/WinHAB.Tests/Core/Mvvm/WaiterViewModelTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WinHAB.Core.Mvvm;
+
+namespace WinHAB.Tests.Core.Mvvm
+{
+  [TestClass]
+  public class WaiterViewModelTests
+  {
+    [TestMethod]
+    public void WaiterViewModelShowHideTests()
+    {
+      var vm = new WaiterViewModel();
+      vm.Show();
+      Assert.IsTrue(vm.IsVisible);
+      vm.Hide();
+      Assert.IsFalse(vm.IsVisible);
+      
+      var message = "Hello world!";
+      vm.Show(message);
+      Assert.IsTrue(vm.IsVisible);
+      Assert.AreEqual(message, vm.Text);
+      vm.Hide();
+      Assert.IsNull(vm.Text);
+    }
+  }
+}
