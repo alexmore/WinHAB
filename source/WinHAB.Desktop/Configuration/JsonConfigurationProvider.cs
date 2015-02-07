9e5cfbf449348259e1634109bbebc0728b5c67e6
alexmore <alexandr.a.mordvinov@gmail.com>
2015-02-08 00:29:13 +0600

    [Desktop] Added FileExtensions for async file read/write, Added JsonConfigurationProvider

---
+++ source/WinHAB.Desktop/Configuration/JsonConfigurationProvider.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using WinHAB.Core.Configuration;
+using WinHAB.Desktop.IO;
+
+namespace WinHAB.Desktop.Configuration
+{
+  public class JsonConfigurationProvider : IConfigurationProvider
+  {
+    private Dictionary<string, string> _values = new Dictionary<string, string>();
+    private readonly string _fileName;
+
+    public JsonConfigurationProvider(string fileName)
+    {
+      _fileName = fileName;
+    }
+
+    public void Load()
+    {
+      if (!File.Exists(_fileName))
+        Save();
+      _values = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(_fileName));
+    }
+
+    public async Task LoadAsync()
+    {
+      var f = new FileInfo(_fileName);
+      if (!f.Exists)
+        await SaveAsync();
+
+      var fileContent = await f.ReadAllTextAsync();
+      _values = await Task.Run(() => JsonConvert.DeserializeObject<Dictionary<string, string>>(fileContent));
+    }
+
+    public void Save()
+    {
+      File.WriteAllText(_fileName, JsonConvert.SerializeObject(_values));
+    }
+
+    public async Task SaveAsync()
+    {
+      var serialized = await Task.Run(() => JsonConvert.SerializeObject(_values));
+      await new FileInfo(_fileName).WriteAllTextAsync(serialized, FileMode.Create);
+    }
+
+    public string Get(string key)
+    {
+      if (!_values.ContainsKey(key)) return null;
+
+      return _values[key];
+    }
+
+    public void Set(string key, string value)
+    {
+      _values[key] = value;
+    }
+  }
+}
---
+++ source/WinHAB.Desktop/IO/FileExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinHAB.Desktop.IO
+{
+  public static class FileExtensions
+  {
+    public static async Task<string> ReadAllTextAsync(this FileInfo file)
+    {
+      using (var sourceStream = new FileStream(file.FullName,
+        FileMode.Open, FileAccess.Read, FileShare.Read,
+        bufferSize: 4096, useAsync: true))
+      {
+        var sb = new StringBuilder();
+
+        var buffer = new byte[0x1000];
+        int numRead;
+        while ((numRead = await sourceStream.ReadAsync(buffer, 0, buffer.Length)) != 0)
+        {
+          string text = Encoding.Unicode.GetString(buffer, 0, numRead);
+          sb.Append(text);
+        }
+
+        return sb.ToString();
+      }
+    }
+
+    public static async Task WriteAllTextAsync(this FileInfo file, string text, FileMode mode)
+    {
+      var encodedText = Encoding.Unicode.GetBytes(text);
+
+      using (var sourceStream = new FileStream(file.FullName,
+        mode, FileAccess.Write, FileShare.None,
+        bufferSize: 4096, useAsync: true))
+      {
+        await sourceStream.WriteAsync(encodedText, 0, encodedText.Length);
+      }
+    }
+  }
+}
