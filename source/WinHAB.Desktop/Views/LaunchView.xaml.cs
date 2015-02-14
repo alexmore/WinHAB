9fc25cab6e3c8b52e41e45168cc5cbc3811c420c
alexmore <alexandr.a.mordvinov@gmail.com>
2015-02-14 22:12:01 +0600

    [Desktop]<WIP> Fixed LaunchView on server textbox key down event, missed key filtering

--- source/WinHAB.Desktop/Views/LaunchView.xaml.cs
+++ source/WinHAB.Desktop/Views/LaunchView.xaml.cs
@@ -28,9 +28,12 @@ namespace WinHAB.Desktop.Views
 
     private void ConnectToServer(object sender, KeyEventArgs e)
     {
-      var cmd = ConnectButton.Command;
-      if (cmd.CanExecute((sender as TextBox).Text))
-        cmd.Execute((sender as TextBox).Text);
+      if (e.Key == Key.Enter)
+      {
+        var cmd = ConnectButton.Command;
+        if (cmd.CanExecute((sender as TextBox).Text))
+          cmd.Execute((sender as TextBox).Text);
+      }
     }
   }
 }
