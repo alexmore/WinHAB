c673a1d3f3d6fefb674550ffdb9ca4f0b814570f
alexmore <alexandr.a.mordvinov@gmail.com>
2015-02-17 14:00:30 +0600

    [Core] Moved CollectionExtension to Fx subfolder

--- source/WinHAB.Core/Fx/CollectionExtensions.cs
+++ source/WinHAB.Core/Fx/CollectionExtensions.cs
@@ -2,7 +2,7 @@
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 
-namespace WinHAB.Core.Collections
+namespace WinHAB.Core.Fx
 {
   public static class CollectionExtensions
   {
