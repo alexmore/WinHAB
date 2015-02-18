e6a21a6780b3a4d83abf516df63a7cb20ddd09f2
alexmore <alexandr.a.mordvinov@gmail.com>
2015-02-18 13:54:52 +0600

    [Desktop] Renamed LaunchView to Views.Pages.BootstrapperPage

--- source/WinHAB.Desktop/Views/Pages/BootstrapperPage.xaml.cs
+++ source/WinHAB.Desktop/Views/Pages/BootstrapperPage.xaml.cs
@@ -1,29 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using WinHAB.Core.Fx.Mvvm;
 using WinHAB.Core.ViewModels.Pages;
 
-namespace WinHAB.Desktop.Views
+namespace WinHAB.Desktop.Views.Pages
 {
   /// <summary>
   /// Interaction logic for LaunchView.xaml
   /// </summary>
   [ViewModel(typeof(BootstrapperPageModel))]
-  public partial class LaunchView : IView
+  public partial class BootstrapperPage : IView
   {
-    public LaunchView()
+    public BootstrapperPage()
     {
       InitializeComponent();
     }
