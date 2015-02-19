ae26bf0ad7a4d40dd2d108aacf5ca0d248f6a64c
alexmore <alexandr.a.mordvinov@gmail.com>
2015-02-20 00:28:05 +0600

    [Tests] Added BootstrapperPageModel tests

---
+++ source/WinHAB.Tests/Core/ViewModels/ViewModelsTestHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Moq;
+using WinHAB.Core.Configuration;
+using WinHAB.Core.Fx.Mvvm;
+using WinHAB.Core.Net;
+using WinHAB.Core.ViewModels.Pages;
+
+namespace WinHAB.Tests.Core.ViewModels
+{
+  public class ViewModelsTestHelper
+  {
+    public ViewModelsTestHelper()
+    {
+      ConfigurationProvider = new FakeConfigurationProvider();
+      Configuration = new AppConfiguration(ConfigurationProvider);
+      
+      ClientFactoryMock = new Mock<IRestClientFactory>();
+      RestClientMock = new Mock<IRestClient>();
+      ClientFactoryMock.Setup(x => x.Create()).Returns(() => RestClientMock.Object);
+
+      NavigationMock = new Mock<INavigationService>();
+      //NavigationMock.Setup(x=>x.ShowMessage(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<Action>()))
+      //  .Verifiable();
+    }
+
+    public FakeConfigurationProvider ConfigurationProvider { get; set; }
+    public AppConfiguration Configuration { get; set; }
+    
+    public Mock<IRestClientFactory> ClientFactoryMock { get; set; }
+    public Mock<IRestClient> RestClientMock { get; set; }
+
+    public Mock<INavigationService> NavigationMock { get; set; }
+  }
+}
---
+++ source/WinHAB.Tests/FakeConfigurationProvider.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WinHAB.Core.Configuration;
+
+namespace WinHAB.Tests
+{
+  public class FakeConfigurationProvider : IConfigurationProvider
+  {
+    public FakeConfigurationProvider()
+    {
+      Values = new Dictionary<string, string>();
+    }
+
+    public Dictionary<string, string> Values { get; set; }
+
+    public virtual void Load() { }
+    public virtual Task LoadAsync() { return Task.FromResult(0); }
+    public virtual void Save() { }
+    public virtual Task SaveAsync() { return Task.FromResult(0); }
+
+    public string Get(string key)
+    {
+      if (!Values.ContainsKey(key)) return null;
+      
+      return Values[key];
+    }
+
+    public void Set(string key, string value)
+    {
+      Values[key] = value;
+    }
+  }
+}
