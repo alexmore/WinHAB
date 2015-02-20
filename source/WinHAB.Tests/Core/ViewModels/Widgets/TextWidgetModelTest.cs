57603bc933c6ca3316cff7cc4397fc3f0ca0ea4b
alexmore <alexandr.a.mordvinov@gmail.com>
2015-02-20 14:26:06 +0600

    [Tests] Added TextWidgetModelTest

---
+++ source/WinHAB.Tests/Core/ViewModels/Widgets/TextWidgetModelTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using WinHAB.Core.Models;
+using WinHAB.Core.ViewModels;
+using WinHAB.Core.ViewModels.Widgets;
+
+namespace WinHAB.Tests.Core.ViewModels.Widgets
+{
+  [TestFixture]
+  public class TextWidgetModelTest
+  {
+    [TestCase(null, Result = WidgetSize.Meduim)] // Null
+    [TestCase("", Result = WidgetSize.Meduim)]
+    // > 15 chars - Wide
+    [TestCase("[1234567890123456]", Result = WidgetSize.Wide)]
+    // > 31 chars - Large
+    [TestCase("[12345678901234567890123456789012]", Result = WidgetSize.Large)]
+    public WidgetSize Constructor_SetsWidgetSize_DependingOnValueLength(string label)
+    {
+      var tw = new TextWidgetModel(new Widget() {Label = label});
+      return tw.Size;
+    }
+  }
+}
