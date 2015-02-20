4d80c6321f75ee742c0bb10ac919dbc882303859
alexmore <alexandr.a.mordvinov@gmail.com>
2015-02-20 13:44:29 +0600

    [Test] Added WidgetModelBase tests

---
+++ source/WinHAB.Tests/Core/ViewModels/WidgetModelBaseTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using WinHAB.Core.Models;
+using WinHAB.Core.ViewModels;
+
+namespace WinHAB.Tests.Core.ViewModels
+{
+  [TestFixture]
+  public class WidgetModelBaseTest
+  {
+    class TestWidgetModel : WidgetModelBase
+    {
+      public TestWidgetModel(Widget data) : base(data)
+      {
+      }
+    }
+
+    [Test]
+    public void Constructor_NotThrowsException_WhenLinkedPageIsNull()
+    {
+      Assert.That(() => new TestWidgetModel(new Widget()), Throws.Nothing);
+
+    }
+
+    [Test]
+    public void Constructor_NotThrowsException_WhenLinkedPageLinkIsNull()
+    {
+      Assert.That(() => new TestWidgetModel(new Widget() { LinkedPage = new Page() }), Throws.Nothing);
+
+    }
+  }
+}
