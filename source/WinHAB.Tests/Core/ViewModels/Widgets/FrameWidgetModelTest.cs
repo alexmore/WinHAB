90dc0629b8ce41de125725af1f44297b427901b3
alexmore <alexandr.a.mordvinov@gmail.com>
2015-02-20 14:19:22 +0600

    [Tests] Added FrameWidgetModelTest

---
+++ source/WinHAB.Tests/Core/ViewModels/Widgets/FrameWidgetModelTest.cs
@@ -0,0 +1,29 @@
+﻿using Moq;
+using NUnit.Framework;
+using WinHAB.Core.Models;
+using WinHAB.Core.ViewModels;
+using WinHAB.Core.ViewModels.Widgets;
+
+namespace WinHAB.Tests.Core.ViewModels.Widgets
+{
+  [TestFixture]
+  public class FrameWidgetModelTest
+  {
+    [Test]
+    public void Cleanup_InvokesCleanup_ForEveryItemInWidgetsCollection()
+    {
+      var f = new FrameWidgetModel(new Widget());
+
+      var wMock1 = new Mock<WidgetModelBase>(new Widget());
+      var wMock2 = new Mock<WidgetModelBase>(new Widget());
+
+      f.Widgets.Add(wMock1.Object);
+      f.Widgets.Add(wMock2.Object);
+
+      f.Cleanup();
+
+      wMock1.Verify(x=>x.Cleanup());
+      wMock2.Verify(x=>x.Cleanup());
+    }
+  }
+}
