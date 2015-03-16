384a1c48bd988157ce8a7bcf39be8808c8b19574
alexmore <alexandr.a.mordvinov@gmail.com>
2015-03-16 16:19:24 +0600

    [Core.Models] Added LabelTag.Parse and tag indexer

--- source/WinHAB.Core/Models/LabelTag.cs
+++ source/WinHAB.Core/Models/LabelTag.cs
@@ -1,4 +1,7 @@
-﻿namespace WinHAB.Core.Models
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace WinHAB.Core.Models
 {
   public class LabelTag
   {
@@ -11,6 +14,53 @@
       Value = value;
     }
 
-    public string Value { get; set; }
+    private string _value;
+
+    public string Value
+    {
+      get { return _value; }
+      set { _value = value; Parse(value); }
+    }
+
+    
+
+    public string this[string key]
+    {
+      get
+      {
+        if (key == null) return null;
+        string res = null;
+        Items.TryGetValue(key, out res);
+        return res;
+      }
+      set
+      {
+        if (string.IsNullOrWhiteSpace(key)) return;
+        Items[key] = value;
+      }
+    }
+
+    private readonly Dictionary<string, string> _items = new Dictionary<string, string>();
+    public Dictionary<string, string> Items
+    {
+      get { return _items; }
+    }
+
+    public void Parse(string labelTagString)
+    {
+      Items.Clear();
+
+      foreach (var i in labelTagString.Split(',')
+        .Select(i => i.Split(':'))
+        .Where(keyValue => keyValue.Length > 1 && !string.IsNullOrWhiteSpace(keyValue[0])))
+      {
+        Items[i[0].Trim()] = string.Join(":", i.Skip(1)).Trim();
+      }
+    }
+
+    public override string ToString()
+    {
+      return Value;
+    }
   }
 }
---
+++ source/WinHAB.Tests/Core/Models/LabelTagTest.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using WinHAB.Core.Models;
+using WinHAB.Core.ViewModels;
+
+namespace WinHAB.Tests.Core.Models
+{
+  [TestFixture]
+  public class LabelTagTest
+  {
+    [Test]
+    public void Items_IsNotNullAfeterCreate()
+    {
+      var lt = new LabelTag();
+      Assert.That(lt.Items, Is.Not.Null);
+    }
+
+    [Test]
+    public void Value_ParsedOnSet()
+    {
+      var lt = new LabelTag();
+      lt.Parse("key1:val1,  key2 : val2");
+      Assert.That(lt.Items, Is.Not.Empty);
+    }
+
+    [TestCase("key1", Result = "val1")]
+    [TestCase("key2", Result = "val2")]
+    [TestCase("key3", Result = "")]
+    public string Parse_IsValid(string key)
+    {
+      var lt = new LabelTag("key1:val1,     key2   : val2, key3 : ,");
+      return lt.Items[key];
+    }
+
+    [TestCase("key1", Result = "val1")]
+    [TestCase("key2", Result = "val2")]
+    [TestCase("key3", Result = "")]
+    [TestCase("", Result = null)]
+    [TestCase(null, Result = null)]
+    public string AccessToItemsGet_IsValid(string key)
+    {
+      var lt = new LabelTag("key1:val1,     key2   : val2, key3 : ,");
+      return lt[key];
+    }
+
+    [TestCase("key1", "val1", Result = "val1")]
+    [TestCase("", "val1", Result = null)]
+    [TestCase(null, "val1", Result = null)]
+    public string AccessToItemsSet_IsValid(string key, string value)
+    {
+      var lt = new LabelTag();
+      lt[key] = value;
+      return lt[key];
+    }
+  }
+}
