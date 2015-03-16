b9e6236485d624b304946af13dad9315c7ddab93
alexmore <alexandr.a.mordvinov@gmail.com>
2015-03-16 19:16:51 +0600

    [Core.Models] Added WidgetPropertiesExtensions - extension methods for converting values to internal types, Added WidgetPropertiesExtensions.GetSize returns WidgetSize for key "Size"

---
+++ source/WinHAB.Core/Models/WidgetPropertiesExtensions.cs
@@ -0,0 +1,23 @@
+﻿using WinHAB.Core.ViewModels;
+
+namespace WinHAB.Core.Models
+{
+  public static class WidgetPropertiesExtensions
+  {
+    public static WidgetSize? GetSize(this WidgetProperties props)
+    {
+      var sizeString = props["Size"];
+      if (sizeString != null)
+      {
+        switch (sizeString.ToLower())
+        {
+          case "medium" : return WidgetSize.Meduim;
+          case "wide" : return WidgetSize.Wide;
+          case "large" : return WidgetSize.Large;
+        }
+      }
+
+      return null;
+    }
+  }
+}
---
+++ source/WinHAB.Tests/Core/Models/WidgetPropertiesExtensionsTest.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using WinHAB.Core.Models;
+using WinHAB.Core.ViewModels;
+
+namespace WinHAB.Tests.Core.Models
+{
+  [TestFixture]
+  public class WidgetPropertiesExtensionsTest
+  {
+    [SetUp]
+    public void SetUp()
+    {
+      wProps = new WidgetProperties();
+    }
+
+    private WidgetProperties wProps;
+
+    [Test]
+    public void GetSize_ReturnsValidValue()
+    {
+      wProps["Size"] = "Medium";
+      Assert.That(wProps.GetSize(), Is.EqualTo(WidgetSize.Meduim));
+      wProps["Size"] = "Wide";
+      Assert.That(wProps.GetSize(), Is.EqualTo(WidgetSize.Wide));
+      wProps["Size"] = "Large";
+      Assert.That(wProps.GetSize(), Is.EqualTo(WidgetSize.Large));
+    }
+
+    [Test]
+    public void GetSize_ReturnsValidValueCaseIsensetive()
+    {
+      wProps["SiZe"] = "MeDiUm";
+      Assert.That(wProps.GetSize(), Is.EqualTo(WidgetSize.Meduim));
+      wProps["SiZe"] = "WiDe";
+      Assert.That(wProps.GetSize(), Is.EqualTo(WidgetSize.Wide));
+      wProps["SiZe"] = "LaRgE";
+      Assert.That(wProps.GetSize(), Is.EqualTo(WidgetSize.Large));
+    }
+  }
+}
